c014c1aa7dc2bab2bf14f61a912f41c5251e0722
russr123 <russr123@yahoo.com>
2020-01-24 14:28:05 -0500

    Create SCCM-ChangeForcedReboot2Non-forced.ps1

---
+++ SCCM-ChangeForcedReboot2Non-forced.ps1
@@ -0,0 +1,6 @@
+#CANCEL a pending reboot
+#For more info, see my Blog post https://sccmf12twice.com/2019/05/sccm-reboot-decoded-how-to-make-a-pc-cancel-start-extend-or-change-mandatory-reboot-to-non-mandatory-on-the-fly/
+
+Set-Itemproperty -path 'HKLM:\SOFTWARE\Microsoft\SMS\Mobile Client\Reboot Management\RebootData' -name 'RebootBy' -value 0;
+shutdown -a 
+Restart-Service ccmexec -force
