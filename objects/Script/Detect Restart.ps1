abc87fc65ff64846cac4c5b9eed3ab3c1e64e5ef
Matthew Hudson <37191708+Mhudson97@users.noreply.github.com>
2020-09-16 10:15:40 -0500

    Automated commit

---
+++ objects/Script/Detect Restart.ps1
@@ -0,0 +1,28 @@
+﻿# Show Machine restart time by Matthew Hudson, MVP
+Function Check-Registry ($Regkey,$Regvalue)
+{
+$Foundit = Get-ItemProperty $Regkey $Regvalue -ErrorAction SilentlyContinue 
+if (($Foundit -eq $null) -or ($Foundit.Length -eq 0))
+        {
+             Exit           
+        }
+        
+}
+
+
+Check-Registry ("HKLM:\SOFTWARE\Microsoft\SMS\Mobile Client\Reboot Management\RebootData","RebootBy")
+
+$UTCEPOCH = Get-ItemPropertyValue -path "HKLM:\SOFTWARE\Microsoft\SMS\Mobile Client\Reboot Management\RebootData" -name RebootBy
+
+
+
+$REstartseconds =  (Get-CimInstance -Namespace root\ccm\policy\machine\requestedconfig -ClassName CCM_RebootSettings).RebootCountdown
+
+If ((Get-TimeZone).SupportsDaylightSavingTime) {$TOffset=3600}
+$newTime = $UTCEPOCH + (Get-TimeZone).BaseUTCOffset.totalSeconds+ $TOffset + $REstartseconds
+
+$UTCTime = (Get-Date 01.01.1970)+([System.TimeSpan]::fromseconds($newTime))
+"$UTCTime UTC" 
+
+$ClientLocaltime =  [System.TimeZoneInfo]::ConvertTimeFromUtc((Get-Date 01.01.1970)+([System.TimeSpan]::fromseconds($newTime)), (Get-TimeZone))
+"$ClientLocaltime Client Local time "  
