4fa43d1c8d08953a7042b8637df3de9afb3c0057
Adam Gross <20917262+AdamGrossTX@users.noreply.github.com>
2020-12-11 12:35:45 -0600

    Automated commit

---
+++ objects/Script/Copy Missing File to Device.ps1
@@ -0,0 +1,18 @@
+﻿#Copy missing file to device
+#Adam Gross
+#https://www.ASquareDozen.com
+
+param(
+    $MissingFileFullPath,
+    $SourceFileFullPath,
+    [bool]$OverwriteExisting
+)
+
+try {
+    if($OverwriteExisting -or !(Test-Path $MissingFileFullPath -ErrorAction SilentlyContinue)) {
+        Copy-Item -Path $SourceFileFullPath -Destination $MissingFileFullPath -Force
+    }
+}
+catch {
+    throw $_
+}
